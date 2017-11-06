412a888dead18ccfcb7caac3cfecfc669ea8d4c3
dokechin <ttatsumi@ra2.so-net.ne.jp>
2017-11-07 07:46:26 +0900

    Add poteto salad

--- public/js/dish.js
+++ public/js/dish.js
@@ -38,10 +38,30 @@ var dish1 = {
 	{name : '白飯', amount : 150, unit : 'グラム'}
 ],
 steps : [
-	  {name : '野菜を切る', duration : 8, content : '一口サイズに野菜を切ります', resources : ['料理人','作業台']},
+	  {name : '野菜を切る', duration : 2, content : '一口サイズに野菜を切ります', resources : ['料理人','作業台']},
 	  {name : '肉野菜を炒める', dependsOn : [0], duration : 5, content : '鍋で炒める', resources : ['料理人','コンロ'], constant : true},
 	  {name : '煮る', dependsOn : [1], duration : 10, content: '野菜がやわらなくなるまで煮ます', resources : ['コンロ'], constant : true },
 	  {name : 'カレールーを溶かす', dependsOn : [2], duration : 5, content : '火を止めてから、カレールーを入れます', resources : ['料理人','コンロ'], constant : true}
   ]};
-    
-  var dishes = [dish1, dish2, dish3];
+
+	var dish4 = {
+		name : 'ポテトサラダ',
+		ingredients : [
+		{name : 'じゃがいも', amount : 1, unit : '個'},
+		{name : 'にんじん',    amount : "1/8", unit : '本'},
+		{name : 'きゅうり', amount : "1/4", unit : '本'},
+		{name : 'ハム', amount : 1, unit : '枚'},
+		{name : '塩', amount : 0.5, unit : 'グラム'},
+		{name : 'こしょう', amount : 0.2, unit : 'グラム'},
+		{name : 'マヨネーズ', amount : 1, unit : '小さじ'},
+		{name : '酢', amount : 1, unit : '小さじ'}
+	],
+	steps : [
+			{name : '材料を切る', duration : 2, content : 'じゃがいもを洗い、半分に切ります。にんじんは皮をむき一口大に切ります。ハムを適当に切ります。', resources : ['料理人','作業台']},
+			{name : 'じゃがいもを茹でる', dependsOn : [0], duration : 10, content : '鍋でじゃがいもを柔らかくなるまで茹でます', resources : ['コンロ'], constant : true},
+			{name : 'にんじんを茹でる', dependsOn : [0], duration : 5, content: 'にんじんを茹でます', resources : ['コンロ'], constant : true },
+			{name : 'じゃがいもを潰します', dependsOn : [1], duration : 1, content : 'じゃがいもを潰します', resources : ['料理人','作業台']},
+			{name : '混ぜる', dependsOn : [2,3], duration : 1, content : 'じゃがいも、にんじん、ハムを混ぜて。調味料で味を整えます。', resources : ['料理人','作業台']}
+		]};
+	
+  var dishes = [dish1, dish2, dish3, dish4];
